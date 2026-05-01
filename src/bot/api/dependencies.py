ed1bf0fe6491a86268ddfca45d766f3657dad7db
raufh10 <raufhamidy10@gmail.com>
2026-05-01 17:38:12 +0000

    adjustments to dockerfile 4

--- src/bot/api/dependencies.py
+++ src/bot/api/dependencies.py
@@ -3,5 +3,5 @@ from core.config import settings
 
 async def verify_webhook_secret(request: Request) -> None:
   token = request.headers.get("X-Telegram-Bot-Api-Secret-Token")
-  if token != settings.WEBHOOK_SECRET:
+  if token != settings.webhook_secret:
     raise HTTPException(status_code=403, detail="Invalid secret token")
